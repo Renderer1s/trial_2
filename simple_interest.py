b5aab64521f136b7c59c4b77604aece49a75e3bf
Renderer1s <83955578+Renderer1s@users.noreply.github.com>
2021-05-10 18:03:45 +0530

    Add files via upload

---
+++ simple_interest.py
@@ -0,0 +1,11 @@
+p = input('Enter value of p = ') # p contains a string value
+n = input('Enter the years = ')
+r = input('Enter the rate of interest =')
+
+p = int(p) # here the sring value of the p is converted into an integer
+n= int(n)
+r = int(r)
+
+si = p*n*r/100
+
+print(si)
